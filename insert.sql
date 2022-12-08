44e12306e18a57d771c60239365ce3e02b916999
geon00 <jgn159753@naver.com>
2022-12-08 12:56:43 +0900

    2022.12.08 SQL 기초 수업

---
+++ insert.sql
@@ -0,0 +1,77 @@
+/* 
+ Create Read Update Delete(CRUD)
+ 
+ INSERT(추가) - C
+ SELECT(조회) - R
+ UPDATE(수정) - U
+ DELETE(삭제) - D
+ 
+ 카멜표기벅 사용x 스네이크 표기법 사용
+ DB에서는 '가 문자열이다.
+ 파란글씨 기준으로 잘라 줄바꿈 / 컬럼이 많은 경우 ,다음 줄바꿈
+ */
+ 
+ INSERT INTO product_mst
+ /* (id, product_name, product_price) VALUES에 순서대로 들어가 있으면 생략가능 */
+ VALUES
+ (0, '스타벅스 블랙  머그컵221', 20000),
+ (0, '스타벅스 화이트  머그컵221', 15000),
+ (0, '스타벅스 블루  머그컵221', 20000);
+ 
+ SELECT/*선택, 입력순서가 중요*/
+  	id,
+  	product_name,
+  	product_price
+ FROM product_mst;
+ 
+ INSERT INTO student_mst
+ VALUES
+ (0, '임지현', 1, 80), 
+ (0, '신경수', 2, 90),
+ (0, '고희주', 3, 75),
+ (0, '문승주', 1, 85),
+ (0, '장건녕', 2, 95),
+ (0, '박수현', 3, 90),
+ (0, '황석민', 1, 80),
+ (0, '박준현', 2, 85),
+ (0, '이승아', 3, 90),
+ (0, '김수현', 1, 75);
+ 
+SELECT
+ 	id,
+ 	`name`, /*`중복되는 이름이 있을경우 사용 */
+ 	student_year,
+ 	score
+FROM
+ 	student_mst
+WHERE
+ 	student_year = 1 and score > 80 /* 데이터를 조회시 사용 */ /* or 사용 예 */  	
+ 	OR (student_year = 3 AND score = 75);
+ 	
+ /*`중복되는 이름이 있을경우 사용 */
+
+WHERE
+	`name` LIKE '%수%' AND `name` NOT LIKE '%수';
+	/* %는 앞에 뭐가오든지 상관없다는 표시, ! / not은 부정하는 의미*/
+	
+	
+
+WHERE
+	student_year not IN (1, 2);
+	
+	
+WHERE
+	score not BETWEEN 80 AND 90; /* between ~사시에 있는 값들 불러오기 */
+
+
+SELECT
+ 	id,
+ 	`name`, 
+ 	student_year,
+ 	score
+FROM
+ 	student_mst
+WHERE
+	`name` is NOT NULL;
+	
+	
